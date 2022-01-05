7c33e64f52b216768c7796f06a2541663fa84b1b
Alex Noble-James <alexnoble143.an@gmail.com>
2022-01-04 21:36:06 -0500

    began initial code for JS game

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+var playerName = window.prompt("What is your robot's name?");
+
+// Creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight()
+    
